9b8c7bf1a844f7a3ffa194360619a042cf454813
Francesco De Carlo <decarlof@gmail.com>
2023-01-15 20:15:35 -0600

    update

--- setup.py
+++ setup.py
@@ -1,17 +1,30 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, find_namespace_packages
+
+# setup(
+#     name='dmm2bm',
+#     version=open('VERSION').read().strip(),
+#     #version=__version__,
+#     author='Francesco De Carlo',
+#     author_email='decarlof@gmail.com',
+#     url='https://github.com/decarlof/dmm',
+#     packages=find_namespace_packages(where="dmm2bm"),
+#     package_dir={"": "dmm2bm"},
+#     include_package_data = True,
+#     scripts=['bin/dmm'],
+#     description='ops',
+#     zip_safe=False,
+# )
 
 setup(
     name='dmm2bm',
     version=open('VERSION').read().strip(),
+    #version=__version__,
     author='Francesco De Carlo',
     author_email='decarlof@gmail.com',
-    url='https://github.com/xray-imaging/dmm2bm',
-    packages=find_packages(where="dmm2bm"),
-    package_dir={"": "dmm2bm"},
-    package_data={"dmm2bm": ["*.json", ]},
+    url='https://github.com/decarlof/dmm',
+    packages=find_packages(),
     include_package_data = True,
     scripts=['bin/dmm'],
-    description='Utitlity to change the DMM energy at 2-BM',
+    description='ops',
     zip_safe=False,
 )
-
