894456d54c5ab18a76872a03dcc04eb9bc9bbb4c
Francesco De Carlo <decarlof@gmail.com>
2023-02-01 17:43:31 -0600

    added shutter open/close support and improved comments/readme file

--- src/dmm2bm/move.py
+++ src/dmm2bm/move.py
@@ -6,8 +6,8 @@ from dmm2bm import log
 from dmm2bm import util
 from dmm2bm import pvs
 
-ShutterA_Open_Value  = 1
-ShutterA_Close_Value = 0
+# OpenShutterValue  = 1
+# CloseShutterValue = 0
 
 def aps2bm(pos_dmm_energy_select, params):
 
@@ -34,11 +34,9 @@ def aps2bm(pos_dmm_energy_select, params):
         log.warning('     *** testing mode: A-shutter will be closed during energy change')
     else:
         log.warning('closing A-shutter')
-        epics_pvs['ShutterA_Close'].put(1, wait=True)
-        # uncomment and test with beam:
-        util.wait_pv(epics_pvs['ShutterA_Move_Status'], ShutterA_Close_Value)
-        log.info('     *** close_shutter A: Done!')
-
+        # epics_pvs['CloseShutter'].put(1, wait=True)
+        close_frontend_shutter(epics_pvs)
+    
     log.info('move motors')
     energy = list(pos_dmm_energy_select.keys())[0]
     for key in epics_pvs:
@@ -57,7 +55,7 @@ def aps2bm(pos_dmm_energy_select, params):
                     time.sleep(.1) 
 
         except KeyError: 
-            log.error('When using an intepolated position the PV associated to motor: %s is not moved' % epics_pvs[key].pvname)
+            log.error('PV associated to motor: %s is missing from the pre-set positon. This motor will not moved' % epics_pvs[key].pvname)
     
     log.info('     *** all move commands sent to motors')
 
@@ -65,5 +63,100 @@ def aps2bm(pos_dmm_energy_select, params):
         epics_pvs['energy_mode'].put(params.mode, wait=True)
         epics_pvs['energy'].put(energy, wait=True)
 
+    log.info('dmm: waiting on motion to complete')
+    while True:
+        time.sleep(.3)
+        if epics_pvs['AllDoneA'].get() and epics_pvs['AllDoneB'].get():
+            break
+    log.info('motion completed')
+    open_frontend_shutter(epics_pvs)
+
     return move_status
+
+
+def open_frontend_shutter(epics_pvs):
+    """Opens the shutters to collect flat fields or projections.
+
+    This does the following:
+
+    - Checks if we are in testing mode. If we are, do nothing else opens the 2-BM front-end shutter.
+
+    """
+    if not epics_pvs['OpenShutter'] is None:
+        pv = epics_pvs['OpenShutter']
+        value = epics_pvs['OpenShutterValue'].get(as_string=True)
+
+        status = epics_pvs['ShutterStatus'].get(as_string=True)
+        log.info('shutter status: %s' % status)
+        log.info('open shutter: %s, value: %s' % (pv, value))
+        epics_pvs['OpenShutter'].put(value, wait=True)
+        wait_frontend_shutter_open(epics_pvs)
+        # wait_pv(epics_pvs['ShutterStatus'], 1)
+        status = epics_pvs['ShutterStatus'].get(as_string=True)
+        log.info('shutter status: %s' % status)
+
+
+
+def wait_frontend_shutter_open(epics_pvs, timeout=-1):
+    """Waits for the front end shutter to open, or for ``abort_scan()`` to be called.
+
+    While waiting this method periodically tries to open the shutter..
+
+    Parameters
+    ----------
+    timeout : float
+        The maximum number of seconds to wait before raising a ShutterTimeoutError exception.
+
+    Raises
+    ------
+    ScanAbortError
+        If ``abort_scan()`` is called
+    ShutterTimeoutError
+        If the open shutter has not completed within timeout value.
+    """
+
+    start_time = time.time()
+    pv = epics_pvs['OpenShutter']
+    value = epics_pvs['OpenShutterValue'].get(as_string=True)
+    value = epics_pvs['OpenShutterValue'].get(as_string=True)
+
+    log.info('open shutter: %s, value: %s' % (pv, value))
+    elapsed_time = 0
+    while True:
+        if epics_pvs['ShutterStatus'].get() == int(value):
+            log.warning("Shutter is open in %f s" % elapsed_time)
+            return
+        value = epics_pvs['OpenShutterValue'].get()
+        time.sleep(1.0)
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+        log.warning("Waiting on shutter to open: %f s" % elapsed_time)
+        epics_pvs['OpenShutter'].put(value, wait=True)
+        if timeout > 0:
+            if elapsed_time >= timeout:
+               exit()
+
+def close_frontend_shutter(epics_pvs):
+    """Closes the shutters to collect dark fields.
+    This does the following:
+
+    - Closes the 2-BM front-end shutter.
+
+    """
+    # if epics_pvs['Testing'].get():
+    #     log.warning('In testing mode, so not opening shutters.')
+    # else:
+    if 1==1:
+        # Close 2-BM front-end shutter
+        if not epics_pvs['CloseShutter'] is None:
+            pv = epics_pvs['CloseShutter']
+            value = epics_pvs['CloseShutterValue'].get(as_string=True)
+            status = epics_pvs['ShutterStatus'].get(as_string=True)
+            log.info('shutter status: %s'% status)
+            log.info('close shutter: %s, value: %s'% (pv, value))
+            epics_pvs['CloseShutter'].put(value, wait=True)
+            util.wait_pv(epics_pvs['ShutterStatus'], 0)
+            status = epics_pvs['ShutterStatus'].get(as_string=True)
+            log.info('shutter status: %s' % status)
+
 
--- src/dmm2bm/pvs.py
+++ src/dmm2bm/pvs.py
@@ -9,28 +9,49 @@ def init(params):
 
     log.info("Inizializing epics PVs")
     
+
     epics_pvs = {}
-    # 2-BM motion PVs (will be used to interpolate positions)
+    # This python tool box relies on the following EPICS PVs served by a different IOC:
+    # $(P)$(R)EnergyMoveXPVName (X=0, 1 ...) hosting the PV name of motors that will be used to move to interpolated positions.
+    # The motor position for $(P)$(R)EnergyMoveXPVName must be present in both lookup table entries for the energy below/above the selected value,
+    # if one of the values is missing the motor will not be moved.  
     epics_move_pvs = init_pvs(params, 'EnergyMove', 'energy move ', n=16)
-    # 2-BM control PVs (with 0-1 positions, will not be used by interpolation, will not be saved in status)
+    # $(P)$(R)EnergyPosXPVName (X=0, 1 ...) hosting the PV name of motors that will NOT be used to move to interpolated positions
+    # These motors will move only when a pre-calibrated energy is selected
     epics_pos_pvs  = init_pvs(params, 'EnergyPos',  'energy pos ',  n=3)
 
     epics_pvs = {**epics_move_pvs, **epics_pos_pvs}
 
-    # 2-BM shutter control PVs
-    epics_pvs['ShutterA_Open']            = PV('2bma:A_shutter:open.VAL')
-    epics_pvs['ShutterA_Close']           = PV('2bma:A_shutter:close.VAL')
-    epics_pvs['ShutterA_Move_Status']     = PV('PA:02BM:STA_A_FES_OPEN_PL')
+    # PV hosting the shutter PV Open/Close/Status PV names
+    epics_pvs['OpenShutter']       = PV(PV(params.energyioc_prefix + 'OpenShutter'    + 'PVName').get())
+    epics_pvs['CloseShutter']      = PV(PV(params.energyioc_prefix + 'CloseShutter'   + 'PVName').get())
+    epics_pvs['ShutterStatus']     = PV(PV(params.energyioc_prefix + 'ShutterStatus'  + 'PVName').get())
+    
+    # PV hosting the value to Open/Close the shutter
+    epics_pvs['CloseShutterValue'] = PV(params.energyioc_prefix    + 'CloseShutter'   + 'Value' )
+    epics_pvs['OpenShutterValue']  = PV(params.energyioc_prefix    + 'OpenShutter'    + 'Value' )
 
-    # 2-BM output PVs
+    # PVs to store the energy value and the energy mode
     epics_pvs['energy']                   = PV(params.energyioc_prefix + 'Energy.VAL')
     epics_pvs['energy_mode']              = PV(params.energyioc_prefix + 'EnergyMode.VAL')
 
+
+    # These are optional PV to store the motion all done. 
+    # These are only used before the open_frontend_shutter() to confirm all motors are done moving
+    # Temporary hardcoded. 
+    epics_pvs['AllDoneA']                 = PV('2bma:alldone')
+    epics_pvs['AllDoneB']                 = PV('2bmb:alldone')
+
+    # Wait 1 second for all PVs to connect
+    time.sleep(1)
+    
     return epics_pvs
 
 def init_pvs(params, pv_prefix='EnergyMove', label='energy move ', n=16):
 
-    pv_name = {}
+    # this python toolbox relies on epics PVs provided by an external IOC.
+    # These PV host 
+    # pv_name = {}
     epics_pvs = {}
     s = ''
     for i in range(n):
