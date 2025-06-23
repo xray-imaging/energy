59454c48132a3932becec8291975e82002b96ecc
Francesco De Carlo <decarlof@gmail.com>
2025-06-22 19:50:11 -0500

    forced a safe state at reboot: Testing mode and pre-calibrated energy modes

--- iocBoot/iocEnergy_2BM/st.cmd
+++ iocBoot/iocEnergy_2BM/st.cmd
@@ -20,3 +20,8 @@ dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=$(P)")
 iocInit
 
 create_monitor_set("auto_settings.req", 30, "P=$(P),R=$(R)")
+
+# Force default values after autosave restore
+dbpf("2bm:Energy:EnergyCalibrationUse", 0)
+# Force default values after autosave restore
+dbpf("2bm:Energy:EnergyTesting", 1)
